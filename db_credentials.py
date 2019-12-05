48a2a451aae57f6f3b2bac3dacec876993af47de
Vishnu Preet <vishnupreet.rp@gmail.com>
2019-12-05 11:21:20 +0530

    Update db_credentials.py

--- db_credentials.py
+++ db_credentials.py
@@ -1,36 +1,12 @@
 from variables import datawarehouse_name
 
-# sql-server (target db, datawarehouse)
+# mysql (target db, datawarehouse)
 datawarehouse_db_config = {
     'user': 'root',
-    'password': 'vishnu',
+    'password': 'password',
     'host': 'localhost',
     'database': 'transactions',
   }
-
-
-# sqlite
-sqlite_db_config = [
-  {
-#    'user': 'your_user_1',
-#    'password': 'your_password_1',
-#    'host': r'C:\Users\vishn\roxy.db',
-    'database': r'C:\Users\vishn\roxy.db',
-  }
-]
-
-# sql-server (source db)
-sqlserver_db_config = [
-  {
-    'Trusted_Connection': 'yes',
-    'driver': '{SQL Server}',
-    'server': 'your_sql_server',
-    'database': 'db1',
-    'user': 'your_db_username',
-    'password': 'your_db_password',
-    'autocommit': True,
-  }
-]
 
 # mysql
 mysql_db_config = [
@@ -46,13 +22,4 @@ mysql_db_config = [
 #    'host': 'db_connection_string_2',
 #    'database': 'db_2',
 #  },
-]
-
-# firebird
-fdb_db_config = [
-  {
-    'dsn': "/your/path/to/source.db",
-    'user': "your_username",
-    'password': "your_password",
-  }
 ]
