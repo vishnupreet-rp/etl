eca4043fc0a0f1cd9333ec03093cb187e5cee7af
Vishnu Preet <vishnupreet.rp@gmail.com>
2019-12-05 11:30:37 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,11 +1,1 @@
 datawarehouse_name = 'transactions'
-column_names = ['bookinginfoid', 'userid', 'usersessionid', 'movieid', 'moviename', 'genre', 'language', 'cityid',
-                'cinemaid', 'cinemaname', 'screenname', 'rating', 'bookingid', 'vistabookingno', 'vistatransno', 'sessionid',
-                'noofseats', 'showdate', 'showtime', 'ticketamount', 'foodamount', 'totalamount', 'bookingdate', 'seatinfo',
-                'tickettypecode', 'ticketdescription', 'vistastatus', 'recheckvistastatus', 'pgcross', 'pgstatus', 'refundstatus',
-                'emailstatus', 'smsstatus', 'recheckemailstatus', 'rechecksmsstatus', 'seatdetails', 'fooddesc', 'PaymentMessage',
-                'VistaRecheckMessage', 'SMSResponse', 'Platform', 'offeramount', 'couponapplied', 'pgtoken', 'cardnumber',
-                'resendcount', 'offerno', 'offertype', 'offeravailable', 'vatavailable', 'vatvalue', 'vatpercentage', 'pgcrosstime',
-                'pgresponsetime', 'pgsourcefrom', 'pgresponse', 'whatnewtype', 'PaymentMethod', 'gccardnumber', 'gcbalance',
-                'gcafterbalance', 'gcpayment', 'Partialamount', 'screenid', 'experience', 'DiscountAmount', 'currentbalance',
-                'beforebalance', 'ticketdiscount', 'fnbdiscount', 'loyaltytier', 'EarnedPoint', 'ReedemPoint', 'Type']
