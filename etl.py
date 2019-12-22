f77d0141b4ae012a87ea42af3da49a995102e954
pmoo <prabakarmoorthy@msn.com>
2019-12-21 19:20:02 -0800

    Update etl.py

--- etl.py
+++ etl.py
@@ -12,6 +12,9 @@ def etl(query, source_cnx, target_cnx):
     source_cursor.execute(query.extract_query)
     data = source_cursor.fetchall()
     source_cursor.close()
+    # -------QQ:
+    # It is good to catch and log duration for getting the data from source
+    # -------
 
     # load data into warehouse db
     if data:
@@ -23,6 +26,10 @@ def etl(query, source_cnx, target_cnx):
         # target_cursor.execute("USE {}".format(datawarehouse_name))
         target_cursor.executemany(query.load_query, data)
         target_cnx.commit()
+        # -------QQ:
+        # Measure duration elapsed in milliseconds for Target write operation
+        # So that you know where you are spending mode of your time
+        # -------        
         end_time = datetime.now()
         print('data loaded to warehouse db')
         target_cursor.execute('''SELECT highest_id FROM log_table WHERE job_status='success' ORDER BY batch_id DESC LIMIT 1''')
@@ -48,6 +55,10 @@ def etl(query, source_cnx, target_cnx):
 
 def etl_process(queries, target_cnx, source_db_config, db_platform):
     # establish source db connection
+    # -------QQ:
+    # Check for input params are not null
+    # Throw specic err so you know which input is not valid
+    # -------    
     if db_platform == 'mysql':
         source_cnx = pymysql.connect(**source_db_config)
     elif db_platform == 'sqlserver':
@@ -58,10 +69,18 @@ def etl_process(queries, target_cnx, source_db_config, db_platform):
         source_cnx = sqlite3.connect(**source_db_config)
     else:
         return 'Error! unrecognised db platform'
-
+    # -------QQ:
+    # You don't want to throw exception? Why string return value?
+    # I haven't done Python and more of a learning question
+    # -------
+    
     # loop through sql queries
     for query in queries:
         etl(query, source_cnx, target_cnx)
 
     # close the source db connection
+    # -------QQ:
+    # After closing the cursor, can't immed. close source connection?
+    # Do you have to keep it open until Target write Op. is complete?
+    # -------
     source_cnx.close()
