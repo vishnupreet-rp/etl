c7550253aa4e2251d612b3fa24c0bf15603807bd
pmoo <prabakarmoorthy@msn.com>
2019-12-21 19:31:38 -0800

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,1 +1,6 @@
+# --------QQ:
+# WHAT you mean by transactions
+# All are transactions - it is always good to have a qualifier 
+# for ex: RevenueTransactions/SupportTicketTransactions/BillingTransactions/etc.,
+# -------
 datawarehouse_name = 'transactions'
