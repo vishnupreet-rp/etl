bce9f74b9fdb66838bea72ef76ce1c528a274834
pmoo <prabakarmoorthy@msn.com>
2019-12-21 19:33:02 -0800

    Update db_credentials.py

--- db_credentials.py
+++ db_credentials.py
@@ -1,5 +1,7 @@
 from variables import datawarehouse_name
-
+# ---------QQ
+# ---- Do not keep creds in source code
+# ---------
 # mysql (target db, datawarehouse)
 datawarehouse_db_config = {
     'user': 'root',
